82aeec5fedba67a39ad1330c83396f9693f6bf0f
guilhermesena1 <desenabr@usc.edu>
2020-11-29 00:12:46 -0800

    adding a mismatch lookup table to count two mismatches at a time

--- src/dna_four_bit_bisulfite.hpp
+++ src/dna_four_bit_bisulfite.hpp
@@ -1,11 +1,13 @@
 #ifndef _DNA_FOUR_BIT_BISULFITE
 #define _DNA_FOUR_BIT_BISULFITE
 #include <stdint.h>
-// A: 0001 = 1
-// C = 0010 = 2
-// G: 0100 = 4
-// T: 1010 = 10
-const uint8_t encode_base_t_rich[256] = {
+/* encoding of ASCII characters into T-rich bases, used
+ * in encoding reads.
+ * A: 0001 = 1
+ * C = 0010 = 2
+ * G: 0100 = 4
+ * T: 1010 = 10 */
+static const uint8_t encode_base_t_rich[256] = {
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //0
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //17
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //33
@@ -24,11 +26,12 @@ const uint8_t encode_base_t_rich[256] = {
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
 
-// A: 0101 = 5
-// C: 0010 = 2
-// G: 0100 = 4
-// T: 1000 = 8
-const uint8_t encode_base_a_rich[256] = {
+/* encoding of ASCII characters into A-rich bases
+ * A: 0101 = 5
+ * C: 0010 = 2
+ * G: 0100 = 4
+ * T: 1000 = 8 */
+static const uint8_t encode_base_a_rich[256] = {
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //0
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //17
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, //33
@@ -46,4 +49,35 @@ const uint8_t encode_base_a_rich[256] = {
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
+
+/* this look-up counts how many mismatches exist between
+ * a read byte, which encodes two read bases as the lookups
+ * above and the reference byte, which encodes two bases
+ * per byte in traditional four-letter DNA. When performing an
+ * and (&&) operation between read byte and reference byte, this
+ * look-up table simply states whether the first four bits are
+ * zero (one mismatch) and whether the last four bits are zero
+ * (two mismatches). With this, we can count two mismatches at
+ * a time when iterating through read and reference.*/
+static const uint8_t mismatch_lookup[] = {
+  2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
+
+
 #endif
