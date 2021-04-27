4d54cdd9ef72091737608e89f5c740488c9a1be5
guilhermesena1 <desenabr@usc.edu>
2021-04-26 22:35:51 -0700

    removing the 'mismatch lookup' previously used on full_compare

--- src/dna_four_bit_bisulfite.hpp
+++ src/dna_four_bit_bisulfite.hpp
@@ -4,7 +4,7 @@
 /* encoding of ASCII characters into T-rich bases, used
  * in encoding reads.
  * A: 0001 = 1
- * C = 0010 = 2
+ * C: 0010 = 2
  * G: 0100 = 4
  * T: 1010 = 10 */
 static const uint8_t encode_base_t_rich[256] = {
@@ -49,35 +49,4 @@ static const uint8_t encode_base_a_rich[256] = {
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 };
-
-/* this look-up counts how many mismatches exist between
- * a read byte, which encodes two read bases as the lookups
- * above and the reference byte, which encodes two bases
- * per byte in traditional four-letter DNA. When performing an
- * and (&&) operation between read byte and reference byte, this
- * look-up table simply states whether the first four bits are
- * zero (one mismatch) and whether the last four bits are zero
- * (two mismatches). With this, we can count two mismatches at
- * a time when iterating through read and reference.*/
-static const uint8_t mismatch_lookup[] = {
-  2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-};
-
-
-
 #endif
