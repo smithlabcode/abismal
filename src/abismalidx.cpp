97d0847dca0cfa69f2c439ed6e1a5a661d72f1c1
Andrew D Smith <andrewds@usc.edu>
2026-03-27 13:07:52 -0700

    src/abismalidx.cpp: changing cli interface to cli11

--- src/abismalidx.cpp
+++ src/abismalidx.cpp
@@ -13,13 +13,23 @@
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+static constexpr auto about = "index for another bisulfite mapping algorithm";
+static constexpr auto description = R"(
+Examples:
+
+abismal idx -t 32 hg38.fa hg38.idx
+)";
 
 #include "abismalidx.hpp"
 
 #include "AbismalIndex.hpp"
 
-#include "OptionParser.hpp"
+#include "CLI11/CLI11.hpp"
 
 #include <config.h>
 
@@ -35,53 +45,50 @@
 int
 abismalidx(int argc, char *argv[]) {  // NOLINT(*-c-arrays)
   try {
-    const std::string version_str =
-      std::string{"(v"} + VERSION + std::string{")"};
-    const std::string description = "build abismal index " + version_str;
-
     std::string target_regions_file;
-    bool verbose = false;
+    bool verbose{};
     std::size_t n_threads = 1;
+    std::string genome_file;
+    std::string outfile;
 
-    /****************** COMMAND LINE OPTIONS ********************/
-    OptionParser opt_parse(argv[0],  // NOLINT(*-pointer-arithmetic)
-                           description, "<genome-fasta> <abismal-index-file>",
-                           2);
-    opt_parse.set_show_defaults();
-    opt_parse.add_opt("targets", 'A', "target regions", false,
-                      target_regions_file);
-    opt_parse.add_opt("threads", 't', "number of threads", false, n_threads);
-    opt_parse.add_opt("verbose", 'v', "print more run info", false, verbose);
+    CLI::App app{about};
+    argv = app.ensure_utf8(argv);
+    app.usage("Usage: abismal idx [OPTIONS] input output");
+    if (argc >= 2)
+      app.footer(description);
+    app.get_formatter()->label("REQUIRED", "REQ");
+    app.get_formatter()->long_option_alignment_ratio(0.2);
+    app.get_formatter()->enable_footer_formatting(false);
 
-    std::vector<std::string> leftover_args;
-    opt_parse.parse(argc, argv, leftover_args);
-    if (argc == 1 || opt_parse.help_requested()) {
-      std::cerr << opt_parse.help_message() << '\n';
-      std::cerr << opt_parse.about_message() << '\n';
+    // clang-format off
+    app.add_option("input", genome_file,
+                   "reference genome in FASTA format (gzip ok)")
+      ->option_text("FILE REQUIRED")
+      ->required()
+      ->check(CLI::ExistingFile);
+    app.add_option("output", outfile,
+                   "abismal index file")
+      ->option_text("FILE REQUIRED")
+      ->required();
+    // ->check(CLI::NonexistentPath);
+    app.add_option("-A,--targets", target_regions_file,
+                   "target regions (BED format)")
+      ->option_text("FILE")
+      ->check(CLI::ExistingFile);
+    app.add_option("-t,--threads", n_threads, "number of threads")
+      ->option_text("UINT")
+      ->check(CLI::PositiveNumber);
+    app.add_flag("-v,--verbose", verbose, "print more run info");
+    // clang-format on
+    if (argc < 2) {
+      std::cout << app.help() << '\n';
       return EXIT_SUCCESS;
     }
-    if (opt_parse.about_requested()) {
-      std::cerr << opt_parse.about_message() << '\n';
-      return EXIT_SUCCESS;
-    }
-    if (opt_parse.option_missing()) {
-      std::cerr << opt_parse.option_missing_message() << '\n';
-      return EXIT_SUCCESS;
-    }
-    if (leftover_args.size() != 2) {
-      std::cerr << opt_parse.help_message() << '\n';
-      return EXIT_SUCCESS;
-    }
-    const std::string genome_file = leftover_args.front();
-    const std::string outfile = leftover_args.back();
-    /****************** END COMMAND LINE OPTIONS *****************/
+    CLI11_PARSE(app, argc, argv);
 
     const auto start_time{std::chrono::high_resolution_clock::now()};
     AbismalIndex::set_n_threads(n_threads);
     AbismalIndex::VERBOSE = verbose;
-
-    if (!std::filesystem::exists(genome_file))
-      throw std::runtime_error("file not found: " + genome_file);
 
     if (!std::filesystem::is_regular_file(genome_file))
       throw std::runtime_error("not regular file: " + genome_file);
